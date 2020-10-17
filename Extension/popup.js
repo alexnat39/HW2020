7f73b82df4e3dc34ecf6231b9287b9f866bf7455
SohumT <70512473+SohumT@users.noreply.github.com>
2020-10-18 04:08:15 +0530

    Update popup.js

--- Extension/popup.js
+++ Extension/popup.js
@@ -1,4 +1,3 @@
-src='https://cdn.rawgit.com/naptha/tesseract.js/1.0.10/dist/tesseract.js'
 document.getElementById("Submit").addEventListener("click", function() {loadFile()})
 function loadFile() {
 console.log("to");
