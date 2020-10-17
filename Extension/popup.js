0a7c4e18cc140954f0e5ae591422b349dfdf4a48
nasr00 <70342009+nasr00@users.noreply.github.com>
2020-10-17 17:42:08 -0400

    Update popup.js

--- Extension/popup.js
+++ Extension/popup.js
@@ -1,12 +1,17 @@
-var loadFile = function(event) {
+document.getElementById("Submit").addEventListener("click", function() {loadFile()})
+function loadFile() {
+console.log("to");
 	var image = document.getElementById('output');
-	image.src = URL.createObjectURL(event.target.files[0]);
-    document.getElementById("Resize").style.display="block";
-	document.getElementById("Resize").addEventListener ("click", function() {
-    
-});
-	document.getElementById("Translate").style.display="block";
-	document.getElementById("Translate").addEventListener ("click", function() {
-    
-});
+	var fileInput = document.getElementById("fileInput");
+	if(fileInput.files[0]) {
+		image.src = URL.createObjectURL(fileInput.files[0]);
+		document.getElementById("Resize").style.display = "block";
+		document.getElementById("Resize").addEventListener("click", function () {
+
+		});
+		document.getElementById("Translate").style.display = "block";
+		document.getElementById("Translate").addEventListener("click", function () {
+
+		});
+	}
 };
