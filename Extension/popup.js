9cb5b46f137182fcde270360b9405cc6258ea0b0
nasr00 <70342009+nasr00@users.noreply.github.com>
2020-10-17 14:56:12 -0400

    Update popup.js

--- Extension/popup.js
+++ Extension/popup.js
@@ -1,1 +1,18 @@
+var loadFile = function(event) {
+    var image = document.getElementById('output');
+    image.src = URL.createObjectURL(event.target.files[0]);
+    var resizeButton = document.createElement("button");
+    resizeButton.innerHTML = "Resize";
+    var body = document.getElementsByTagName("body")[0];
+    body.appendChild(resizeButton);
+    resizeButton.addEventListener ("click", function() {
 
+    });
+    var translateButton = document.createElement("button");
+    translateButton.innerHTML = "Translate";
+    var body = document.getElementsByTagName("body")[0];
+    body.appendChild(translateButton);
+    translateButton.addEventListener ("click", function() {
+
+    });
+};
