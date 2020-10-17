2dd7c103066638a1281ac4a64ae5dc84fbf43d0b
Alex Natalenko <66867853+alexnat39@users.noreply.github.com>
2020-10-18 02:48:36 +0300

    Create popup.js

---
+++ image_resizer/popup.js
@@ -0,0 +1,24 @@
+document.getElementById("Submit").addEventListener("click", function() {loadFile()})
+function loadFile() {
+    console.log("to");
+    var image = document.getElementById('output');
+    var fileInput = document.getElementById("fileInput");
+
+
+    if(fileInput.files[0]) {
+        //calls either scan&translate function or the resize dimension function
+        image.src = URL.createObjectURL(fileInput.files[0]);
+        document.getElementById("Resize").style.display = "block";
+        document.getElementById("Resize").addEventListener("click", function () {
+            location.href = "imageResizer.html";
+        });
+        document.getElementById("Translate").style.display = "block";
+        document.getElementById("Translate").addEventListener("click", function () {
+            Tesseract.recognize(image).then(function(result){
+                console.log(result.text);
+                window.alert(result.text);
+            });
+
+        });
+    }
+};
