17a064ea32d6f5f2980ed19401978904017be0c6
Alex Natalenko <66867853+alexnat39@users.noreply.github.com>
2020-10-18 03:47:31 +0300

    Update imageResizer.js

--- image_resizer/imageResizer.js
+++ image_resizer/imageResizer.js
@@ -1,49 +1,58 @@
 var image;
+//uploads the image to the preview container
+document.addEventListener("DOMContentLoaded", () => {
+    const recentImageDataURL = localStorage.getItem("current-image");
+    if (recentImageDataURL) {
+        document.querySelector("#imagePreview").setAttribute("src", recentImageDataURL);
+    }   else {
+        console.log("no image stored")
+    }
+});
 var loadFile = function(event) {
     image = document.getElementById('output');
     image.src = URL.createObjectURL(event.target.files[0]);
 }
 function resizeDimensions(image, height, width) {
-        var filesToUpload = document.getElementById('image').files;
-        var file = filesToUpload[0];
-        // Create an image
-        var img = document.createElement("img");
-        // Create a file reader
-        var reader = new FileReader();
-        // Set the image once loaded into file reader
-        reader.onload = function(e) {
-            img.src = e.target.result;
+    var filesToUpload = document.getElementById('image').files;
+    var file = filesToUpload[0];
+    // Create an image
+    var img = document.createElement("img");
+    // Create a file reader
+    var reader = new FileReader();
+    // Set the image once loaded into file reader
+    reader.onload = function(e) {
+        img.src = e.target.result;
 
-            var canvas = document.createElement("canvas");
-            var ctx = canvas.getContext("2d");
-            ctx.drawImage(img, 0, 0);
+        var canvas = document.createElement("canvas");
+        var ctx = canvas.getContext("2d");
+        ctx.drawImage(img, 0, 0);
 
-            var MAX_WIDTH = 400;
-            var MAX_HEIGHT = 400;
-            var width = img.width;
-            var height = img.height;
+        var MAX_WIDTH = 400;
+        var MAX_HEIGHT = 400;
+        var width = img.width;
+        var height = img.height;
 
-            if (width > height) {
-                if (width > MAX_WIDTH) {
-                    height *= MAX_WIDTH / width;
-                    width = MAX_WIDTH;
-                }
-            } else {
-                if (height > MAX_HEIGHT) {
-                    width *= MAX_HEIGHT / height;
-                    height = MAX_HEIGHT;
-                }
+        if (width > height) {
+            if (width > MAX_WIDTH) {
+                height *= MAX_WIDTH / width;
+                width = MAX_WIDTH;
             }
-            canvas.width = width;
-            canvas.height = height;
-            var ctx = canvas.getContext("2d");
-            ctx.drawImage(img, 0, 0, width, height);
-
-            var dataurl = canvas.toDataURL("image/png");
-            document.getElementById('output').src = dataurl;
+        } else {
+            if (height > MAX_HEIGHT) {
+                width *= MAX_HEIGHT / height;
+                height = MAX_HEIGHT;
+            }
         }
-        // Load files into file reader
-        reader.readAsDataURL(file);
+        canvas.width = width;
+        canvas.height = height;
+        var ctx = canvas.getContext("2d");
+        ctx.drawImage(img, 0, 0, width, height);
+
+        var dataurl = canvas.toDataURL("image/png");
+        document.getElementById('output').src = dataurl;
+    }
+    // Load files into file reader
+    reader.readAsDataURL(file);
 }
 function getNewHeight() {
     var heightInput = document.getElementById("heightInput").value; //convert them as int
@@ -58,3 +67,4 @@ function getNewWidth() {
 }
 document.getElementById("ResizeFinal").addEventListener("click", function() {resizeDimensions(getNewHeight(), getNewWidth())})
 // .open - opens a new tab
+
