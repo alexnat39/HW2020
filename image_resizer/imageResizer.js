5688e04905cf39167bebb326439ab67587153604
nasr00 <70342009+nasr00@users.noreply.github.com>
2020-10-17 21:34:06 -0400

    Update imageResizer.js

--- image_resizer/imageResizer.js
+++ image_resizer/imageResizer.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const recentImageDataURL = localStorage.getItem("current-image");
     if (recentImageDataURL) {
         document.querySelector("#imagePreview").setAttribute("src", recentImageDataURL);
+        console.log("true")
     }   else {
         console.log("no image stored")
     }
@@ -66,13 +67,6 @@ function getNewWidth() {
 
 }
 //uploads the image to the preview container
-document.addEventListener("DOMContentLoaded", () => {
-    const recentImageDataURL = localStorage.getItem("current-image");
-    if (recentImageDataURL) {
-        document.querySelector("#imagePreview").setAttribute("src", recentImageDataURL);
-    }   else {
-        console.log("no image stored")
-    }
-});
 document.getElementById("ResizeFinal").addEventListener("click", function() {resizeDimensions(getNewHeight(), getNewWidth())})
 // .open - opens a new tab
+
