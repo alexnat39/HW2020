22ce02524d3757f10d8d688f6d38958798f639cf
Alex Natalenko <66867853+alexnat39@users.noreply.github.com>
2020-10-18 06:43:35 +0300

    Update imageResizer.js

--- image_resizer/imageResizer.js
+++ image_resizer/imageResizer.js
@@ -1,38 +1,31 @@
 var image;
+
 //uploads the image to the preview container
 document.addEventListener("DOMContentLoaded", () => {
     const recentImageDataURL = localStorage.getItem("current-image");
     if (recentImageDataURL) {
         document.querySelector("#imagePreview").setAttribute("src", recentImageDataURL);
-        console.log("true")
     }   else {
         console.log("no image stored")
     }
 });
-var loadFile = function(event) {
-    image = document.getElementById('output');
-    image.src = URL.createObjectURL(event.target.files[0]);
-}
-function resizeDimensions(image, height, width) {
-    var filesToUpload = document.getElementById('image').files;
-    var file = filesToUpload[0];
-    // Create an image
-    var img = document.createElement("img");
-    // Create a file reader
-    var reader = new FileReader();
-    // Set the image once loaded into file reader
-    reader.onload = function(e) {
-        img.src = e.target.result;
+// var loadFile = function(event) {
+//     image = document.getElementById('output');
+//     image.src = URL.createObjectURL(event.target.files[0]);
+// }
 
+function resizeDimensions(height, width) {
+    const recentImageDataURL = localStorage.getItem("current-image");
+    image = document.getElementById("imagePreview");
+
+
+    if (recentImageDataURL) {
+        //creates an empty canvas
         var canvas = document.createElement("canvas");
-        var ctx = canvas.getContext("2d");
-        ctx.drawImage(img, 0, 0);
+        var MAX_WIDTH = 600;
+        var MAX_HEIGHT = 600;
 
-        var MAX_WIDTH = 400;
-        var MAX_HEIGHT = 400;
-        var width = img.width;
-        var height = img.height;
-
+        //checks for image height and width not to mess the way they look
         if (width > height) {
             if (width > MAX_WIDTH) {
                 height *= MAX_WIDTH / width;
@@ -44,29 +37,30 @@ function resizeDimensions(image, height, width) {
                 height = MAX_HEIGHT;
             }
         }
+        //setting the new width and height of a canvas as the parameters we passed in
         canvas.width = width;
         canvas.height = height;
-        var ctx = canvas.getContext("2d");
-        ctx.drawImage(img, 0, 0, width, height);
 
-        var dataurl = canvas.toDataURL("image/png");
-        document.getElementById('output').src = dataurl;
+        var ctx = canvas.getContext("2d");
+        ctx.drawImage(image, 0, 0, width, height);
+
+        var resizedImage = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
+        window.location.href = resizedImage;
+
     }
-    // Load files into file reader
-    reader.readAsDataURL(file);
 }
 function getNewHeight() {
-    var heightInput = document.getElementById("heightInput").value; //convert them as int
+    var heightInput = + document.getElementById("heightInput").value; //convert them as int
+    console.log(heightInput);
     alert(heightInput);
     return heightInput;
 }
 function getNewWidth() {
-    var widthInput = document.getElementById("widthInput").value;
+    var widthInput = + document.getElementById("widthInput").value;
     alert(widthInput);
     return widthInput;
 
 }
-//uploads the image to the preview container
+
 document.getElementById("ResizeFinal").addEventListener("click", function() {resizeDimensions(getNewHeight(), getNewWidth())})
 // .open - opens a new tab
-
